01f3a8a217d8bf1d2d2ccf54c3bacf34a15bc905
Eduardo Veloso <deadzapdos@live.com>
2021-05-23 23:37:23 -0300

    labecommerce-final

--- src/App.js
+++ src/App.js
@@ -1,46 +1,51 @@
 import React from 'react';
 import './App.css';
-import ProductCard from './Components/ProductCard/ProductCard';
-import ShoppingItem from './Components/ShoppingItem/ShoppingItem';
+import meteorito from './img/Meteorito.jpeg';
+import foguete from './img/Foguete-Espacial.jpeg';
+import robo from './img/Robo-Humanoide.jpeg';
+import satelite from './img/Satelite.jpeg';
+import traje from './img/Traje-Espacial.jpeg';
+import viagem from './img/Viagem-para-lua.jpeg';
 
 const produtos = [{
   id: 1,
-  name: "Produto 1",
+  name: "Meteorito",
   value: 100,
-  imageUrl: "https://picsum.photos/400/201"},
+  imageUrl: meteorito},
 {
   id: 2,
-  name: "Produto 2",
+  name: "Foguete",
   value: 200,
-  imageUrl: "https://picsum.photos/400/202"},
+  imageUrl: foguete},
 {
   id: 3,
-  name: "Produto 3",
+  name: "Robo Humanóide",
   value: 300,
-  imageUrl: "https://picsum.photos/400/203"},
+  imageUrl: robo},
 {
   id: 4,
-  name: "Produto 4",
+  name: "Satélite",
   value: 400,
-  imageUrl: "https://picsum.photos/400/204"},
+  imageUrl: satelite},
 {
   id: 5,
-  name: "Produto 5",
+  name: "Traje Espacial",
   value: 500,
-  imageUrl: "https://picsum.photos/400/205"},
+  imageUrl: traje},
 {
   id: 6,
-  name: "Produto 6",
+  name: "Viagem Para a Lua",
   value: 600,
-  imageUrl: "https://picsum.photos/400/206"},  
+  imageUrl: viagem},  
 ]
 
 export class App extends React.Component{
   state = {
     valormin: 100, 
     valormax: 300,
-    nomeproduto: "Produto",
+    nomeproduto: "",
     value: 'crescente',
+    carrinhoLista: [],
     }
     // Aqui Handle o change dos filtros
       onChangeMin = (e) => {
@@ -57,6 +62,79 @@ export class App extends React.Component{
       handleChangeDaOrdem = (e) => {
         this.setState({value: e.target.value})
       }
+
+      
+      adicionarListaCarrinho = (produto) => {
+        const produtoDoCarrinho = this.state.carrinhoLista.find((produtoDoCarrinho) => {
+         if (produtoDoCarrinho.id === produto.id) {
+            return true;
+        }
+          return false;
+        });
+  
+        if (!produtoDoCarrinho) {
+          const novoProdutoDoCarrinho = {
+            ...produto,
+            quantidade: 1
+          };
+    
+          const carrinhoNovo = [...this.state.carrinhoLista, novoProdutoDoCarrinho];
+        
+          this.setState({ carrinhoLista: carrinhoNovo });
+        } else {
+          const carrinhoNovo = this.state.carrinhoLista.map((produtoDoCarrinho) => {
+            if (produtoDoCarrinho.id === produto.id) {
+              return {
+                ...produtoDoCarrinho,
+                quantidade: produtoDoCarrinho.quantidade + 1
+              };
+            } else {
+              return produtoDoCarrinho;
+            }
+          });
+          this.setState({ carrinhoLista: carrinhoNovo });
+          // console.log(this.state.carrinhoLista)
+        }
+        
+        }
+
+        removerProduto = (produto) => {
+          const produtoDoCarrinho = this.state.carrinhoLista.find((produtoDoCarrinho) => {
+            if (produtoDoCarrinho.id === produto.id) {
+              return true;
+            }
+             return false;
+          });
+     
+          if (produtoDoCarrinho) {
+             
+            const carrinhoNovo = this.state.carrinhoLista.map((produtoDoCarrinho) => {
+              if (produtoDoCarrinho.id === produto.id) {
+                return {
+                  ...produtoDoCarrinho,
+                  quantidade: produtoDoCarrinho.quantidade - 1
+                };
+              } else {
+                return produtoDoCarrinho;
+              }
+             });
+             
+             this.setState({ carrinhoLista: carrinhoNovo });
+             
+          // console.log(this.state.carrinhoLista)
+          }
+          // const arrayTeste = [...this.state.carrinhoLista]
+          // const teste = (produto, i) => {
+          //   if (produto.quantidade === 0){
+          //     return arrayTeste.splice(i)
+              
+          //   }
+          //   const semZero = teste
+          //   this.setState({ carrinhoLista: arrayTeste });
+          // }
+          // const semZero = carrinhoLista    
+               
+      }        
 
   render(){
 
@@ -81,14 +159,43 @@ export class App extends React.Component{
 
     // Map que fará com o que os produtos apareçam na tela
     const listaDeComponentes = ordem.map((data, i) => (
-      <ProductCard key={i}
-        name = {data.name}
-        value = {data.value}
-        urlImagem = {data.imageUrl}
-       /> 
-     ))
+       <div key={i} className="card">
+       <img src={data.imageUrl} alt={"Imagem"} />
+          <div className="infor-product">
+          <p>{data.name}</p>
+          <p>{data.value}</p>
+          <button onClick={() => this.adicionarListaCarrinho(data)}>Adicionar ao carrinho</button>
+          </div>    
+    </div>
+    ))
 
-     return (
+    
+
+    const produtosCarrinho = this.state.carrinhoLista.map((produto, i) => (
+      <div key={i} className="item">
+          <div className="produto-escolhido">
+            <p>{produto.quantidade}x</p>
+            <p>{produto.name}</p>
+            <p>R${produto.value * produto.quantidade}</p>
+            <button onClick={() => this.removerProduto(produto)}> Remover </button>
+          </div> 
+        </div>
+
+    ))
+
+    const total = this.state.carrinhoLista.reduce(
+      (valorAcumulado, produtoCarrinho) => {
+        const valorProduto = produtoCarrinho.value * produtoCarrinho.quantidade;
+
+        return valorAcumulado + valorProduto;
+      },
+      0
+    );
+
+  return (
+
+    
+
     <div className="App">
         <div className= "Container-Filtro">  
         <h3>Filtros:</h3>
@@ -126,22 +233,23 @@ export class App extends React.Component{
               <option value="decrescente">Decrescente</option>
             </select>
           </label>
-         </div>
-         <div className="container-posts">
+        </div>
+        <div className="container-posts">
 
              {listaDeComponentes}
 
-         </div>
-       </div>
+        </div>
+      </div>
 
-       <div className="Container-ShoppingCart">
-         <h3>Carrinho</h3>
-         
-       </div>
+      <div className="Container-ShoppingCart">
+        <h3>Carrinho:</h3>
 
+            {produtosCarrinho}
+
+          <p>Valor Total: R$ {total}</p> 
+       </div>
      </div>
-         );
-     }
-  }
-  export default App;
+  )}
+}
+export default App;
   
